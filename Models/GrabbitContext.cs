4648911926cd01c12a287dbe0079ffbe9dd07089
Kavyansh Singh <81253296+Kavy01@users.noreply.github.com>
2023-10-25 07:12:31 +0530

    Add files via upload

---
+++ Models/GrabbitContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace API.Models
+{
+    public class GrabbitContext : DbContext
+    {
+        public GrabbitContext(DbContextOptions<GrabbitContext> options)
+           : base(options)
+        {
+        }
+        public virtual DbSet<Book> Books { get; set; }
+        public virtual DbSet<BookCategory> BookCategories { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+    }
+}
